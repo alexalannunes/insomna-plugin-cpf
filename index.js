ea776d9d6fe0a53da7bf58b3af99c90fc39a9b10
Alex Alan Nunes <alexalannunes@gmail.com>
2021-07-15 12:40:30 -0300

    chore: add prettier

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 function cpf(formatted) {
-  let cpf_ = 'xxxxxxxxxxx'.replace(/x/g, function(x) {
-    return Math.random()*9|0
-  })
+  let cpf_ = "xxxxxxxxxxx".replace(/x/g, function (x) {
+    return (Math.random() * 9) | 0;
+  });
 
   if (formatted) {
-    cpf_ = cpf_.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4')
+    cpf_ = cpf_.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
   }
 
   return cpf_;
-
 }
-module.exports = cpf
+module.exports = cpf;
